f05ccac666e39657218383b94405ad81d43ea83b
hp <chhabraashish123@gmail.com>
2020-06-06 01:52:59 +0530

    FireBase Updated

--- app/src/main/java/com/example/languageidentifier/MainActivity.java
+++ app/src/main/java/com/example/languageidentifier/MainActivity.java
@@ -1,12 +1,21 @@
 package com.example.languageidentifier;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.nfc.Tag;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.ml.naturallanguage.FirebaseNaturalLanguage;
+import com.google.firebase.ml.naturallanguage.languageid.FirebaseLanguageIdentification;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,7 +32,28 @@ public class MainActivity extends AppCompatActivity {
         b.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                
+                FirebaseLanguageIdentification languageIdentifier =
+                        FirebaseNaturalLanguage.getInstance().getLanguageIdentification();
+                languageIdentifier.identifyLanguage(e.getText().toString())
+                        .addOnSuccessListener(
+                                new OnSuccessListener<String>() {
+                                    @Override
+                                    public void onSuccess(@Nullable String languageCode) {
+                                        if (languageCode != "und") {
+                                            t.setText(languageCode);
+                                        } else {
+                                            t.setText("Can't identify language.");
+                                        }
+                                    }
+                                })
+                        .addOnFailureListener(
+                                new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(@NonNull Exception e) {
+                                        // Model couldn’t be loaded or other internal error.
+                                        // ...
+                                    }
+                                });
             }
         });
     }
